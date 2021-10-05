2ea22f1ef9d6649e87e470d919b6243526604b92
Radik Abubjazov <abubjazov@mail.ru>
2021-10-05 19:30:39 +0300

    загрузка макета проекта

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import React, {Component} from 'react'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
+class App extends Component {
+  state = {
+    counter: 0
+  }
+
+  updateCounter(value) {
+    this.setState({
+      counter: this.state.counter + value
+    })
+  }
+
+  render() {
+    return (
+      <div className={'App'}>
+        <h1>Счетчик <strong>{this.state.counter}</strong></h1>
+
+        <hr/>
+
+        <div className="Actions">
+          <button onClick={() => this.updateCounter(1)}>Добавить 1</button>
+          <button onClick={() => this.updateCounter(-1)}>Вычесть 1</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
